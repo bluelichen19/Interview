207d75e284ede5861ae2e7b0b5380beb69a46919
bluelichen19 <blueliche_n@163.com>
2015-08-05 19:31:11 +0800

    1

--- Interview.cpp
+++ Interview.cpp
@@ -1,6 +1,56 @@
 #include "Interview.h"
+int printfStr(char* p,int len)
+{
+	if(p == NULL || len <= 0)
+		return -1;
+	int m = 0;
+	while(m<len)
+	{
+		printf("%c",p[m]);
+		m++;
+	}
+	printf("\n");
+	return 0;
+}
+void debugPrintf(const int line,const char* function)
+{
+	printf("debug====%s====%d\n",function,line);
+}
+int reverseStr(char* p,int begin,int end)
+{
+	DEBUGMSG;
+	if(p == NULL || begin < 0 || end < 0)
+		return -1;
+	char tmp;
+	while(begin < end)
+	{
+		tmp = p[begin];
+		p[begin] = p[end];
+		p[end] = tmp;
+		begin++;
+		end--;
+	}
+//	debugPrintf("debug====%s====%d\n",__FUNCTION__,__LINE__);
+	return 0;
+}
+int rotataStr(char* p,int num,int len)
+{
+//	debugPrintf("debug====%s====%d\n",__FUNCTION__,__LINE__);
+	if(p == NULL || len <= 0)
+		return -1;
+	num %= len;
+	reverseStr(p,0,num-1);
+	reverseStr(p,num,len-1);
+	reverseStr(p,0,len-1);
+//	debugPrintf("debug====%s====%d\n",__FUNCTION__,__LINE__);
+	return 0;
+}
 int main(int argc,char* argv[])
 {
+	char p[]={'a','b','c','d','e','f'};
+	printfStr(p,6);
+	int ret = rotataStr(p,2,6);
+	printfStr(p,6);
 	printf("main function run complete!\n");
-	return 0
+	return 0;
 }
--- Interview.h
+++ Interview.h
@@ -1,7 +1,27 @@
-#ifdef		__INTERVIEW__H__
+#ifndef		__INTERVIEW__H__
 #define		__INTERVIEW__H__
 #include <stdio.h>
 #include <string.h>
 #include <memory.h>
+
+#ifndef _DEBUG
+#define _DEBUG
 #endif
 
+//#ifdef _DEBUG
+//#define DEBUGMSG(msg) printf(msg);printf(“%s%d%s”,__DATE__,__LINE__,__FILE__)
+//#else
+//#define DEBUGMSG(msg,date) 
+//#endif
+//
+//#endif
+
+#ifdef _DEBUG
+//#define DEBUGMSG(msg,date) printf(msg);printf(“%d%d%s”,date,_LINE_,_FILE_)
+#define DEBUGMSG printf("msg\n");printf(“%d%d%s”,date,_LINE_,_FILE_)
+
+#else
+      #define DEBUGMSG(msg,date)
+#endif
+
+#endif
