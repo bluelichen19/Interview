7e466a2b0d6d212fc2ea5c80281e115770653902
bluelichen19 <blueliche_n@163.com>
2015-08-05 15:06:38 +0800

    Interview Test
    Interview Test

---
+++ Interview.cpp
@@ -0,0 +1,6 @@
+#include "Interview.h"
+int main(int argc,char* argv[])
+{
+	printf("main function run complete!\n");
+	return 0
+}
---
+++ Interview.h
@@ -0,0 +1,7 @@
+#ifdef		__INTERVIEW__H__
+#define		__INTERVIEW__H__
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+#endif
+
