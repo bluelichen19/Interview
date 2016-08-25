171c68cd6e1bc0ee355e85bdbcc622f6f4bd7ae0
bluelichen19 <blueliche_n@163.com>
2016-08-25 17:38:15 +0800

    0825

--- main.cpp
+++ main.cpp
@@ -14,7 +14,49 @@
       #define DEBUGMSG
       #endif
 
-int main(int argc ,char* argv[])
+void PrintBuffer(void* pBuff, unsigned int nLen)
+{
+    unsigned int i = 0;
+
+    const int nBytePerLine = 16;
+    unsigned char* p = (unsigned char*)pBuff;
+    //char szHex[3*nBytePerLine+1] = {0};
+    char szHex[3*nBytePerLine+1];
+    memset(szHex,0,3*nBytePerLine+1);
+    if (NULL == pBuff || 0 == nLen)
+    {
+        printf("input ptr is NULL\n");
+        return;
+    }
+    printf("-----------------begin-------------------\n");
+    for (i=0; i<nLen; ++i)
+    {
+        int idx = 3 * (i % nBytePerLine);
+        if (0 == idx)
+        {
+            memset(szHex, 0, sizeof(szHex));
+        }
+#ifdef WIN32
+        sprintf_s(&szHex[idx], 4, "%02x ", p[i]);// buff长度要多传入1个字节
+#else
+        snprintf(&szHex[idx], 4, "%02x ", p[i]); // buff长度要多传入1个字节
+#endif
+        
+        // 以16个字节为一行，进行打印
+        if (0 == ((i+1) % nBytePerLine))
+        {
+            printf("%s\n", szHex);
+        }
+    }
+
+    // 打印最后一行未满16个字节的内容
+    if (0 != (nLen % nBytePerLine))
+    {
+        printf("%s\n", szHex);
+    }
+
+    printf("------------------end-------------------\n");
+}int main(int argc ,char* argv[])
 {
     int ret;
     ret = 0;
