84ec9e157f38d63558fb7699a6bceba9a00f3f6a
bluelichen19 <blueliche_n@163.com>
2016-08-25 16:49:11 +0800

    0825

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+
+#ifndef _DEBUG
+#define _DEBUG
+#endif
+
+#ifdef _DEBUG
+////#define DEBUGMSG(msg,date) printf(msg);printf(“%d%d%s”,date,_LINE_,_FILE_)
+#define DEBUGMSG do{printf("debug_info==>");printf("%s line:%d %s <%s>\n",__FUNCTION__,__LINE__,__FILE__,__DATE__);}while(0)
+//
+#else
+      #define DEBUGMSG
+      #endif
+
+int main(int argc ,char* argv[])
+{
+    int ret;
+    ret = 0;
+    DEBUGMSG;
+    return ret;
+}
