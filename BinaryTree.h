edcfc821152d4c96d0c358ba26ca28f01ed2a228
bluelichen19 <blueliche_n@163.com>
2016-08-26 14:40:19 +0800

    add utils

---
+++ BinaryTree.h
@@ -0,0 +1,17 @@
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+
+#pragma once
+
+struct BinaryTreeNode 
+{
+    int                    m_nValue; 
+    BinaryTreeNode*        m_pLeft;  
+    BinaryTreeNode*        m_pRight; 
+};
+
+BinaryTreeNode* CreateBinaryTreeNode(int value);
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight);
+void PrintTreeNode(BinaryTreeNode* pNode);
+void PrintTree(BinaryTreeNode* pRoot);
+void DestroyTree(BinaryTreeNode* pRoot);
